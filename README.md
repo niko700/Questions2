75c05d454a5f2f558468dc513a3782322a3ed6c8
Vera Neroni <vnneroni@gmail.com>
2024-06-06 19:16:56 -0400

    uploading test code for subprocess question

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Questions2
+# 
+
+This is the repository that temporarily holds the code I share when I have a question on the code.
---
+++ subprocess question.py
@@ -0,0 +1,15 @@
+import subprocess
+
+#print doc package
+print("Printing doc package documents")
+
+subprocess.run(["python", "01_doc1.py"])
+print("Creating ouptput 1")
+subprocess.run(["python", "02_doc2.py"])
+print("Creating ouptput 2")
+subprocess.run(["python", "03_doc3.py"])
+print("Creating ouptput 3")
+subprocess.run(["python", "04_doc4.py"])
+print("Creating ouptput 4")
+subprocess.run(["python", "05_doc5.py"])
+print("Creating ouptput 5")
