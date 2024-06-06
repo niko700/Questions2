e76970dc88bb31f93630b597089706e6318fb3d5
Vera Neroni <vnneroni@gmail.com>
2024-06-06 19:36:31 -0400

    updated header notes

--- subprocess question.py
+++ subprocess question.py
@@ -1,15 +1,34 @@
+"""
+This is an example of code that used to work, but does not work anymore.
+
+Setup:
+Each doc.py has code inside that looks for its respective doc1_template.docx, fills it out, and saves a filled out version.
+
+
+Issue:
+When main directory had all the .py files and .docx templates, the following code worked.
+
+New setup is:
+C:\\Users\\user\\Desktop\\MainFolder --> has all .py files
+C:\\Users\\user\\Desktop\\MainFolder\\ChildFolder --> has all the .docx files
+Each .py file has a path to the .docx file when it looks for the template, and this works when run individually.
+This does not work when running this run_all.py from "MainFolder" directory where it is currently saved.
+
+"""
+
 import subprocess
+
 
 #print doc package
 print("Printing doc package documents")
 
-subprocess.run(["python", "01_doc1.py"])
+subprocess.run(["python", "doc1.py"])
 print("Creating ouptput 1")
-subprocess.run(["python", "02_doc2.py"])
+subprocess.run(["python", "doc2.py"])
 print("Creating ouptput 2")
-subprocess.run(["python", "03_doc3.py"])
+subprocess.run(["python", "doc3.py"])
 print("Creating ouptput 3")
-subprocess.run(["python", "04_doc4.py"])
+subprocess.run(["python", "doc4.py"])
 print("Creating ouptput 4")
-subprocess.run(["python", "05_doc5.py"])
+subprocess.run(["python", "doc5.py"])
 print("Creating ouptput 5")
